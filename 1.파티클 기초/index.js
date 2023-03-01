1e165ded453d01592a6151568996ba7368503e44
dnjswlekd <wj8014@gmail.com>
2023-03-01 17:34:53 +0900

    3.1

--- 1.파티클 기초/index.js
+++ 1.파티클 기초/index.js
@@ -4,46 +4,128 @@ const ctx = canvas.getContext("2d"); //사용할 도구
 console.log(window.devicePixelRatio);
 
 const dpr = window.devicePixelRatio;
+let canvasWidth;
+let canvasHeight;
+let particles;
 
-const canvasWidth = 300;
-const canvasHeight = 300;
+function init() {
+  canvasWidth = 1920;
+  canvasHeight = 1920;
 
-canvas.style.width = canvasWidth + "px";
-canvas.style.height = canvasHeight + "px";
+  canvas.style.width = canvasWidth + "px";
+  canvas.style.height = canvasHeight + "px";
 
-canvas.width = canvasHeight * dpr;
-canvas.height = canvasHeight * dpr;
-ctx.scale(dpr, dpr);
+  canvas.width = canvasHeight * dpr;
+  canvas.height = canvasHeight * dpr;
+  ctx.scale(dpr, dpr);
+
+  particles = [];
+
+  const TOTAL = canvasWidth / 10;
+
+  for (let i = 0; i < TOTAL; i++) {
+    const x = randomNumBetween(0, canvasWidth);
+    const y = randomNumBetween(0, canvasHeight);
+    const radius = randomNumBetween(50, 100);
+    const vy = randomNumBetween(1, 5);
+    const particle = new Particle(x, y, radius, vy);
+    particles.push(particle);
+  }
+}
+
+const feGaussianBlur = document.querySelector("feGaussianBlur");
+const feColorMatrix = document.querySelector("feColorMatrix");
+
+const controls = new (function () {
+  this.blurValue = 40;
+  this.alphaChannel = 100;
+  this.alphaOffset = -23;
+  this.acc = 1.03;
+})();
+
+let gui = new dat.GUI();
+
+const f1 = gui.addFolder("Gooey Effect");
+f1.open();
+
+f1.add(controls, "blurValue", 0, 100).onChange((value) => {
+  feGaussianBlur.setAttribute("stdDeviation", value);
+});
+
+f1.add(controls, "alphaChannel", 1, 200).onChange((value) => {
+  feColorMatrix.setAttribute("values", `1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 ${value} ${controls.alphaOffset}`);
+});
+
+f1.add(controls, "alphaOffset", -40, 40).onChange((value) => {
+  feColorMatrix.setAttribute("values", `1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 ${controls.alphaChannel} ${value}`);
+});
+
+const f2 = gui.addFolder("Particle Property");
+f2.open();
+
+f2.add(controls, "acc", 1, 1.5, 0.01).onChange((value) => {
+  particles.forEach((particle) => (particle.acc = value));
+});
 
 //animation 한프레임마다 다음 위치에 그려줘서 모든 프레임을 재생시켜주면 하나의 애니메이션이 됌
 
 class Particle {
-  constructor(x, y, radius) {
+  constructor(x, y, radius, vy) {
     this.x = x;
     this.y = y;
     this.radius = radius;
+    this.vy = vy;
+    this.acc = 1.03;
+  }
+  update() {
+    this.vy *= this.acc;
+    this.y += this.vy;
   }
   draw() {
     ctx.beginPath(); //그리기 시작하는 곳
     ctx.arc(this.x, this.y, this.radius, 0, (Math.PI / 180) * 360);
-    ctx.fillStyle = "red";
-    // ctx.stroke();
+    ctx.fillStyle = "orange";
     ctx.fill();
     ctx.closePath(); //끝나는 곳
   }
 }
 
-const x = 100;
-const y = 100;
-const radius = 50;
-const particle = new Particle(x, y, radius);
+const randomNumBetween = (min, max) => {
+  return Math.random() * (max - min + 1) + min;
+};
+
+let interval = 1000 / 60;
+let now, delta;
+let then = Date.now();
 
 function animate() {
   window.requestAnimationFrame(animate);
+  now = Date.now();
+  delta = now - delta;
+
+  if (delta < interval) return;
 
   ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+  particles.forEach((particle) => {
+    particle.update();
+    particle.draw();
 
-  particle.draw();
+    if (particle.y - particle.radius > canvasHeight) {
+      particle.y = -particle.radius;
+      particle.x = randomNumBetween(0, canvasWidth);
+      particle.radius = randomNumBetween(50, 100);
+      particle.vy = randomNumBetween(1, 5);
+    }
+  });
+
+  then = now - (delta % interval);
 }
 
-animate();
+window.addEventListener("load", () => {
+  init();
+  animate();
+});
+
+window.addEventListener("resize", () => {
+  init();
+});
